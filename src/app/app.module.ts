c729e62ab1702c9c270f51adb016c43b763ab608
Brivaël Brenelière <brivael.breneliere@gmail.com>
2022-12-17 11:48:07 +0100

    ajout de la whitecase animé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomelanderComponent } from './homelander/homelander.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomelanderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomelanderComponent]
 })
 export class AppModule { }
