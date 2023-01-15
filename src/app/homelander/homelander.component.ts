8aa2539e623ca3b309ada072709e06ab4e795511
Brivaël Brenelière <brivael.breneliere@gmail.com>
2023-01-15 14:58:58 +0100

    compenent scss commented

--- src/app/homelander/homelander.component.ts
+++ src/app/homelander/homelander.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
+@Component({ 
   selector: 'app-homelander',
   templateUrl: './homelander.component.html',
-  styleUrls: ['./homelander.component.scss']
+  styleUrls: ['./homelander.component.scss'] 
 })
 export class HomelanderComponent implements OnInit {
 
   constructor() { }
-
+   
   ngOnInit(): void {
   }
 
